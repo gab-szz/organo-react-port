8dc091e6a7c14fafe65dfa2c0ea85249a4c88cec
Gab <125615352+gab-szz@users.noreply.github.com>
2025-08-10 15:23:12 -0400

    Adiciona a dependência 'hex-to-rgba' e implementa funcionalidade para mudar a cor dos times no componente Time

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,19 @@ import { Formulario } from "./components/Formulario";
 import Time from "./components/Time";
 
 function App() {
-  const times = [
+  const [times, setTimes] = useState([
     {
       nome: "Inovação e Gestão",
-      corPrimaria: "#1565c0", // Azul forte
-      corSecundaria: "#e3f2fd", // Azul bem leve
-    },
-    { nome: "Front-End", corPrimaria: "#2e7d32", corSecundaria: "#e8f5e9" }, // Verde forte e leve
-    { nome: "Back-End", corPrimaria: "#4e342e", corSecundaria: "#efebe9" }, // Marrom forte e leve
-    { nome: "Mobile", corPrimaria: "#f9a825", corSecundaria: "#fffde7" }, // Amarelo forte e leve
-    { nome: "Data Science", corPrimaria: "#0277bd", corSecundaria: "#e1f5fe" }, // Azul forte e leve
-    { nome: "UX/UI", corPrimaria: "#ad1457", corSecundaria: "#fce4ec" }, // Rosa forte e leve
-    { nome: "Marketing", corPrimaria: "#6a1b9a", corSecundaria: "#f3e5f5" }, // Roxo forte e leve
-    { nome: "Vendas", corPrimaria: "#ef6c00", corSecundaria: "#fff3e0" }, // Laranja forte e leve
-  ];
+      cor: "#e3f2fd",
+    }, // Azul forte e leve
+    { nome: "Front-End", cor: "#e8f5e9" }, // Verde forte e leve
+    { nome: "Back-End", cor: "#efebe9" }, // Marrom forte e leve
+    { nome: "Mobile", cor: "#fffde7" }, // Amarelo forte e leve
+    { nome: "Data Science", cor: "#e1f5fe" }, // Azul forte e leve
+    { nome: "UX/UI", cor: "#fce4ec" }, // Rosa forte e leve
+    { nome: "Marketing", cor: "#f3e5f5" }, // Roxo forte e leve
+    { nome: "Vendas", cor: "#fff3e0" }, // Laranja forte e leve
+  ]);
 
   const [colaboradores, setColaboradores] = useState<
     { nome: string; cargo: string; imagem: string; time: string }[]
@@ -36,6 +35,17 @@ function App() {
     console.log("Deletar colaborador com ID");
   }
 
+  function mudarCorDoTime(cor: string, timeNome: string) {
+    setTimes(
+      times.map((time) => {
+        if (time.nome === timeNome) {
+          time.cor = cor;
+        }
+        return time;
+      })
+    );
+  }
+
   return (
     <>
       <Banner />
@@ -48,8 +58,8 @@ function App() {
         <Time
           key={time.nome}
           nome={time.nome}
-          corPrimaria={time.corPrimaria}
-          corSecundaria={time.corSecundaria}
+          cor={time.cor}
+          mudarCor={mudarCorDoTime}
           colaboradores={colaboradores.filter(
             (colaborador) => colaborador.time === time.nome
           )}
--- src/components/Time/index.tsx
+++ src/components/Time/index.tsx
@@ -1,5 +1,6 @@
 import Colaborador from "../Colaborador";
 import "./Time.css";
+import hexToRgba from "hex-to-rgba";
 
 interface TimeProps {
   colaboradores: {
@@ -10,9 +11,9 @@ interface TimeProps {
     time: string;
   }[];
   nome: string;
-  corSecundaria: string;
-  corPrimaria: string;
+  cor: string;
   aoDeletar: () => void;
+  mudarCor: (cor: string, nome: string) => void;
 }
 
 const Time = (props: TimeProps) => {
@@ -20,15 +21,21 @@ const Time = (props: TimeProps) => {
     props.colaboradores.length > 0 && (
       <section
         className="time"
-        style={{ backgroundColor: props.corSecundaria }}
+        style={{ backgroundColor: hexToRgba(props.cor, "0.6") }}
       >
-        <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+        <input
+          value={props.cor}
+          onChange={(evento) => props.mudarCor(evento.target.value, props.nome)}
+          type="color"
+          className="input-cor"
+        />
+        <h3 style={{ borderColor: props.cor }}>{props.nome}</h3>
         <div>
           {props.colaboradores.map((colaborador, indice) => {
             return (
               <Colaborador
                 key={indice}
-                corDeFundo={props.corPrimaria}
+                corDeFundo={props.cor}
                 nome={colaborador.nome}
                 cargo={colaborador.cargo}
                 imagem={colaborador.imagem}
